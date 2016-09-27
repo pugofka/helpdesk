3f40da92cc9ba7d4025a12711a14fe93c121fc62
Yushkevich Vitaly <yushkevichv@gmail.com>
2016-09-28 02:24:32 +0300

    some magic with styles from ribbon for new refactor

--- resources/assets/js/components/Clients.vue
+++ resources/assets/js/components/Clients.vue
@@ -1,23 +1,69 @@
 <template>
-
+<div v-if="clients.length != 0">
     <div class="row" >
         <div class="col-xs-12">
             <div class="content-menu">
                 <div class="links">
+                    <a v-for="client in clients" data-id="client_{{client.id}}" href="">
+                        {{client.name}}
+                    </a>
+                </div>
 
-                    <ul>
+                <div class="content">
+                    <div v-for="client in clients" class="item" id="client_{{client.id}}" title="Edit client">
+                        <header>
+                            <div class="client client-info-{{$index}} page-title-section">
+                                <h2 class="pull-left">{{client.name}} <a v-on:click="startClientEditMode($index)" class="show-on-hover btn btn-default" title="Edit Client"><i class="ion-edit"></i></a></h2>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div>
+                                <p><label>Contact name: </label> {{client.point_of_contact}}</p>
+                                <p><label>Contact number: </label> {{client.phone_number}}</p>
+                                <p><label>Contact email: </label> <a href="mailto:{{client.email}}">{{client.email}}</a></p>
+                            </div>
+                        </header>
+                        <hr>
+                        <span v-on:click="showNewProjectForm(client.id, $index)" title="Create new project" class="btn btn-default pull-right">New Project</span>
 
-                    <li v-for="client in clients" >
-                        {{ client.name}}
-                        <strong @click="delete(client)"> X </strong>
-                    </li>
-                    </ul>
+                            <h4>Projects</h4>
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <td>#</td>
+                                    <td>Name</td>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="project in client.projects">
+                                    <td>$index + 1 </td>
+                                    <td><a href="">project.name </a></td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+
+                        <br>
+                        <div class="clearfix"></div>
+                        <hr><br><br>
+                        <span v-on:click="deleteClient(client, $index)" class="btn btn-danger pull-right">Delete {{ client.name }}</span>
+                        <div class="clearfix"></div>
+                    </div>
                 </div>
 
             </div>
 
         </div>
     </div>
+</div>
+
+<div v-if="clients.length == 0">
+    <div class="clearfix"></div>
+    <p class="alert alert-warning">
+        Your clients will be listed here once you create some.
+        Create a new client <a v-on:click="showCreateForm()">now</a>.
+    </p>
+</div>
+
 </template>
 
 <script>
